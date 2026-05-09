2d0213ec37beaa4e28ec31c5495868835957dadc
Jeevan Khatri <jeevankhatri001@gmail.com>
2026-05-09 14:59:17 +0545

    changed

--- intelliplan/settings.py
+++ intelliplan/settings.py
@@ -53,6 +53,11 @@ INSTALLED_APPS = [
     'apps.intellichat.apps.IntelliChatConfig',
     'aboutus',
 ]
+CSRF_TRUSTED_ORIGINS = config(
+    'CSRF_TRUSTED_ORIGINS',
+    default='http://localhost,http://127.0.0.1',
+    cast=lambda v: [s.strip() for s in v.split(',')]
+)
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
